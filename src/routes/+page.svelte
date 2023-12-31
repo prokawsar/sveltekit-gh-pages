7e60e954fc6fd710738d1e27c0cccc9484541f87
Kawsar Ahmed <prokawsar@gmail.com>
2023-12-31 16:05:59 +0600

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,4 @@
+<div class="flex flex-col justify-center items-center w-3/4 mx-auto mt-10">
+	<h1 class="text-3xl font-bold">Welcome to SvelteKit</h1>
+	<p class="text-gray-500">Visit <a class="text-blue-500" href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+</div>
